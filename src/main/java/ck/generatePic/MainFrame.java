042f3f8841d316cc3acadad46c2c3294599aed78
ck <chengkks1987n@126.com>
2015-05-04 21:47:10 +0800

    finish the basic function of generation and save. todo: add the setting function; improve the generate algorithm。

--- src/main/java/ck/generatePic/MainFrame.java
+++ src/main/java/ck/generatePic/MainFrame.java
@@ -1,7 +1,11 @@
 package ck.generatePic;
 
 import java.awt.EventQueue;
+import java.awt.FileDialog;
 
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -15,9 +19,20 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
 
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import javax.swing.border.LineBorder;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+
 public class MainFrame {
-	
+
 	private GeneratePic gp = new GeneratePic();
+	BufferedImage bi = null;
 
 	private JFrame frame;
 	private JTextField textField;
@@ -53,34 +68,56 @@ public class MainFrame {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("文本");
 		lblNewLabel.setBounds(10, 14, 61, 15);
 		frame.getContentPane().add(lblNewLabel);
-		
+
 		textField = new JTextField();
 		textField.setBounds(41, 10, 331, 23);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
-		
+
+		JPanel panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel.setBounds(10, 39, 414, 189);
+		frame.getContentPane().add(panel);
+		panel.setLayout(new BorderLayout(0, 0));
+
+		final JLabel lblPic = new JLabel("");
+		panel.add(lblPic, BorderLayout.CENTER);
+
 		JButton btnGenerate = new JButton("生成");
 		btnGenerate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
-				JOptionPane.showMessageDialog(null, textField.getText());
-				
+				bi = gp.generate(textField.getText());
+				ImageIcon icon = new ImageIcon(bi);
+				lblPic.setIcon(icon);
 			}
 		});
 		btnGenerate.setBounds(372, 10, 62, 23);
 		frame.getContentPane().add(btnGenerate);
-		
+
 		JButton btnSave = new JButton("保存");
+		btnSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (bi != null) {
+					try {
+						JFileChooser save = new JFileChooser();
+						save.setFileFilter(new FileNameExtensionFilter("GIF文件", "gif"));						
+						if (JFileChooser.APPROVE_OPTION == save
+								.showSaveDialog(frame)) {							
+							ImageIO.write(bi, "gif", new File(save.getSelectedFile().getPath()+".gif"));
+						}
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		});
 		btnSave.setBounds(372, 229, 62, 23);
 		frame.getContentPane().add(btnSave);
 	}
-	
-	private void generate(JLabel container, String text){
-		BufferedImage bi = gp.generate(text);
-		
-	}
+
 }
